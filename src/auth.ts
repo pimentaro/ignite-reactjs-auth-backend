e35822100b46af9dcd5e51bf83c30fe294bef678
pimentaro <gigadobrasil@gmail.com>
2021-12-13 07:28:49 -0400

    v2

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,16 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken"
 
-import { auth } from './config';
-import { createRefreshToken } from './database';
+import { auth } from "./config"
+import { createRefreshToken } from "./database"
 
-export function generateJwtAndRefreshToken(email: string, payload: object = {}) {
+export function generateJwtAndRefreshToken(
+  email: string,
+  payload: object = {}
+) {
   const token = jwt.sign(payload, auth.secret, {
     subject: email,
-    expiresIn: 5, // 15 minutes
-  });
+    expiresIn: 10, // 15 minutes
+  })
 
   const refreshToken = createRefreshToken(email)
 
